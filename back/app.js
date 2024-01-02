1c06c21ca93b78801ba8a9c65d72e47c6acb7551
RubenRoman <ruben.roman@forcast.cl>
2024-01-02 17:40:29 -0300

    :bug: add static folder

--- back/app.js
+++ back/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const compression = require('compression');
 const cors = require('cors');
+const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./src/routes/index');
 require('dotenv').config();
@@ -17,6 +18,11 @@ app.use( cors() )
 .use( compression() );
 
 app.use('/api', routes);
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 server.listen( app.get('port'), () => {
     console.log('BACKEND RUNNING ON PORT ', app.get('port'));
