af3f1806329f756b702f3ec8bd8091f1d90e71cd
RubenRoman <ruben.roman@forcast.cl>
2024-01-04 01:21:41 -0300

    :closed_lock_with_key: update environment variables

--- front/src/services/shipping-ways.service.js
+++ front/src/services/shipping-ways.service.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 export default class ShippingWayService {
     
     constructor() {
-        this.url = 'http://34.125.138.36:4000/api/shipping-ways';
+        this.apiUrl = `${import.meta.env.VITE_API_URL}/shipping-ways`;
     }
 
     getAll(){
-        return axios.get( this.url )
+        return axios.get( this.apiUrl )
             .then( response => response.data )
             .catch( err => { throw err } );
     }
