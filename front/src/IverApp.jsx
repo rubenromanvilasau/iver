518e62937ddf0dc28d5af36985bb16d800c27203
RubenRoman <ruben.roman@forcast.cl>
2024-02-03 01:34:57 -0300

    :sparkles: new checkout page

--- front/src/IverApp.jsx
+++ front/src/IverApp.jsx
@@ -1,43 +1,44 @@
 import { Route, Routes } from 'react-router-dom';
-import { HomePage, ItemPage, LoginPage, MyOrdersPage, MyProfilePage, PublishItemPage, SearchResultsPage } from './pages/index';
+import { HomePage, ItemPage, LoginPage, MyOrdersPage, MyProfilePage, CheckoutPage, PublishItemPage, SearchResultsPage } from './pages/index';
 import { Footer, Navbar, ProtectedRoute } from './components/index';
 import { UserProvider } from './context/UserProvider';
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'; 
 
 export const IverApp = () => {
     return(
         <UserProvider>
-        <div className='flex flex-col justify-between'>
-            <Navbar/>
-            <Routes>
-                <Route path='/' element={ <HomePage/> }/>
-                <Route path='/search/:query' element={ <SearchResultsPage/> }/>
-                <Route path='/login' element={ <LoginPage/> }/>
-                <Route path='/item/:id' element={ <ItemPage/> }/>
-                <Route path='/my-profile'
-                    element={
-                        // <ProtectedRoute>
-                            <MyProfilePage/>
-                        // </ProtectedRoute>
-                    }
-                />
-                <Route path='/my-orders' 
-                    element={ 
-                        <ProtectedRoute>
-                            <MyOrdersPage/>
-                        </ProtectedRoute> 
-                }/>
-                <Route 
-                    path='/publish' 
-                    element={ 
-                        // <ProtectedRoute>
-                            <PublishItemPage/>
-                        // </ProtectedRoute> 
-                    }
-                />
-            </Routes>
-            <Footer/>
-        </div>
+            <div className='flex flex-col justify-between'>
+                <Navbar/>
+                <Routes>
+                    <Route path='/' element={ <HomePage/> }/>
+                    <Route path='/search/:query' element={ <SearchResultsPage/> }/>
+                    <Route path='/login' element={ <LoginPage/> }/>
+                    <Route path='/item/:id' element={ <ItemPage/> }/>
+                    <Route path='/my-profile'
+                        element={
+                            // <ProtectedRoute>
+                                <MyProfilePage/>
+                            // </ProtectedRoute>
+                        }
+                    />
+                    <Route path='/my-orders' 
+                        element={ 
+                            <ProtectedRoute>
+                                <MyOrdersPage/>
+                            </ProtectedRoute> 
+                    }/>
+                    <Route 
+                        path='/publish' 
+                        element={ 
+                            // <ProtectedRoute>
+                                <PublishItemPage/>
+                            // </ProtectedRoute> 
+                        }
+                    />
+                    <Route path='/checkout/:id' element={<CheckoutPage/>}/>
+                </Routes>
+                <Footer/>
+            </div>
         </UserProvider>
     )
 }
---
+++ front/src/pages/checkout/CheckoutPage.jsx
@@ -0,0 +1,6 @@
+
+export const CheckoutPage = () => {
+    return (
+        <div className="mt-16 text-text-primary text-2xl">CheckoutPage</div>
+    )
+};
