188ff41af0f65378670832e53eec9c7c12f1ab4c
RubenRoman <ruben.roman@forcast.cl>
2024-03-20 00:32:34 -0300

    :hammer: update seed file to create shipping ways in db

--- back/prisma/seeders/seed.js
+++ back/prisma/seeders/seed.js
@@ -1,24 +1,40 @@
 const { prisma } = requier('../../src/db');
 
 async function main() {
-    const categories = [
-        {
-        name: 'Electronics',
-        description: 'Electronic devices'
-        },
-        {
-        name: 'Books',
-        description: 'Books'
-        },
-        {
-        name: 'Clothes',
-        description: 'Clothes'
-        }
-    ];
+
+    await prisma.category.createMany({
+        data: [
+            {
+            name: 'Electronics',
+            description: 'Electronic devices'
+            },
+            {
+            name: 'Books',
+            description: 'Books'
+            },
+            {
+            name: 'Clothes',
+            description: 'Clothes'
+            }
+        ],
+    });
     
-    for (const category of categories) {
-        await prisma.category.create({ data: category });
-    }
+    await prisma.shippingWay.createMany({
+        data: [
+            {
+                name: 'Personal delivery',
+            },
+            {
+                name: 'External courier',
+            },
+            {
+                name: 'Meet in public place',
+            },
+            {
+                name: 'Pickup in my house',
+            },
+        ]
+    });
 
     console.log('Categories seeded');
     console.log('---------------------------------');    
